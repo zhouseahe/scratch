ac8110923896430126ca61d00f8655bae7d545b3
Seahe <zhouseahe@gmail.com>
2014-11-07 15:46:27 +0800

    Delete users.js

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/user', function(req, res) {
-    res.render('user/share', { title: 'something to say' });
-});
-
-module.exports = router;
